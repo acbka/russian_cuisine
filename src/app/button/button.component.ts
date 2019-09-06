09c40b42d71c9903e07e18bf2b540a6dfe2a456f
acbka <vladochka.vb@gmail.com>
2019-09-06 18:17:41 +1200

    button

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dishes } from '../dishes';
 import { Dish } from '../dish';
+import { count } from 'console';
 
 @Component({
   selector: 'app-button',
@@ -9,17 +10,20 @@ import { Dish } from '../dish';
 })
 export class ButtonComponent implements OnInit {
    dishes : Dish [] = Dishes;
+   count : number = 0;
 
    @Input() dish;
    disabled;
+   
 
    onClick(){
       this.dish.selected = !this.dish.selected;
+
       this.dishes = Dishes.filter(dish => dish.selected == true);
       if (this.dishes.length == 2){
-
       }
-      console.log(this.dish)
+      
+      console.log(this.dishes.length)
    }
 
   constructor() { }
