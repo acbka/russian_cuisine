050cb5535415979d7bad160eb4312d15f536da8c
acbka <vladochka.vb@gmail.com>
2019-09-24 19:12:06 +1200

    pict

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrderService } from '../order.service';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -9,36 +10,80 @@ import { OrderService } from '../order.service';
 })
 export class UserComponent implements OnInit {
 
-   checkoutForm = new FormGroup({
+  checkoutForm = new FormGroup({
+    customerName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      this.forbiddenNameValidator(/bobik/i)]),
+    customerPhone: new FormControl(''),
+    customerEmail: new FormControl(''),
+    customerAddress: new FormControl(''),
+    customerSuburb: new FormControl(''),
+    deliveryDateTime: new FormControl('')
+  });
 
-   customerName : new FormControl('', Validators.required),
-   customerEmail : new FormControl('', Validators.required),
-   customerAddress : new FormControl(''),
-   customerSuburb : new FormControl(''),
-   customerPhone : new FormControl(''),
-   deliveryDateTime : new FormControl(''),
-   })
+  checked : string = "none";
 
-   minDate(): string {
-      var result = (new Date());
-      return result.toLocaleDateString();
-   }
-
-  constructor(private formBuilder: FormBuilder, orderService: OrderService) {
-/*
-   this.checkoutForm = this.formBuilder.group({
-      customerName: '',
-      customerEmail: '',
-      customerAddress: ''
-    });
-*/
-   }
-
-   onSubmit(){
-      console.warn(this.checkoutForm.status);
-   }
-
-  ngOnInit() {
+  forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+    }
   }
 
+  dateFormat(date: Date) : string {
+    let y: number = date.getFullYear();
+
+    let m: number = date.getMonth() + 1;
+    let sm: string = m < 10 ? `0${m}` : `${m}`;
+
+    let d: number = date.getDate();
+    let sd: string = d < 10 ? `0${d}` : `${d}`;
+
+    return `${y}-${sm}-${sd}`;
+  }
+
+  minDate(): string {
+    var result = (new Date());
+
+    if (result.getHours() <= 12) {
+      result.setDate(result.getDate() + 1);
+    } else {
+      result.setDate(result.getDate() + 2);
+    }
+
+    return this.dateFormat(result);
+  }
+
+  maxDate(): string {
+    var result = (new Date());
+    result.setDate(result.getDate() + 7);
+    return this.dateFormat(result);
+  }
+
+  radioClick(show: string) : void {
+     this.checked = show;
+  }
+constructor(
+  private orderServce: OrderService,
+  private formBuilder: FormBuilder) {
+
+  //this.checkoutForm = this.formBuilder.group({
+  //  customerName : "",
+  //  customerPhone : "",
+  //  customerAddress : "",
+  //  deliveryDateTime : new Date()
+  //});
+
 }
+
+onSubmit() {
+  console.log(this.checkoutForm.status);
+}
+
+ngOnInit() {
+}
+
+}
+
+// (new Intl.DateTimeFormat('en-US', options).format(result)).replace('/', '-').replace('/', '-').replace('/', '-');
