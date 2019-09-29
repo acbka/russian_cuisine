d7d220412704d2b9d611c644cbdf0eef6db01d40
acbka <vladochka.vb@gmail.com>
2019-09-29 16:45:09 +1300

    hide

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,12 +16,29 @@ export class HeaderComponent implements OnInit {
       this.burgerShow = this.show ? "navbar-collapse collapse" : "navbar-collapse collapse show";
       this.show = !this.show; 
    }
+
+      //let show = document.getElementsByClassName('collapse')[0];
+  // let menuItems = document.getElementsByClassName('nav-link') || document.getElementsByClassName('dropdown-item');
+  // menuItems.onClick() {
+   //   show.classList.add("collapsed")
+   //   };
+   
 */
 
+   
+   menuHide() : void {
+      let hide = document.getElementsByClassName('collapse')[0];
+      hide.classList.remove('show')
+   }
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+   constructor() { }
+
+   ngOnInit() {
+      let items : HTMLCollection = document.getElementsByClassName('item');
+      for (let i = 0; i < items.length; i++) {
+         items.item(i).addEventListener('click',this.menuHide)
+      }
+   }
 
 }
